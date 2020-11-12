418d091a49ab0d5a65c29814a442fdef3e5fca4a
ZHE-JIA <a8510110981@gmail.com>
2020-11-12 15:53:36 +0800

    輸入發票+資料庫功能

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -1,6 +1,12 @@
 <?php
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
+    $dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
 
-
+    $sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values ('".implode("','",$_POST)."')";
+    echo "$sql";
+    $pdo->exec($sql);
+    echo "新增完成";
+    header("location:../index.php");
 ?>
